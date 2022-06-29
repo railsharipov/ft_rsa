22cfae47cb24bf17f99a0c0bb3ddecaec8e4b492
nugliar <hashan.hall@gmail.com>
2022-06-29 18:34:08 -0500

    change asn primitive type init

--- asn/asn_type_primitive.c
+++ asn/asn_type_primitive.c
@@ -2,29 +2,6 @@
 #include <ssl_asn.h>
 #include <bnum.h>
 
-t_iasn	*asn_primitive_bool(const char *key, const char *name)
-{
-	t_iasn	*item;
-
-	item = asn_item_init();
-	item->type = ft_strdup(key);
-	return (item);
-}
-
-t_iasn	*asn_primitive_int(const char *key, const char *name)
-{
-	t_iasn	*item;
-	size_t	size;
-
-	item = asn_item_init();
-	SSL_ALLOC(item->content, sizeof(t_num));
-	init_num(item->content);
-	size = sizeof(t_num);
-	item->bitsize = NBYTES_TO_NBITS(size);
-	item->type = ft_strdup(key);
-	return (item);
-}
-
 t_iasn	*asn_primitive_ostring(const char *key, const char *name)
 {
 	t_iasn	*item;
@@ -34,22 +11,24 @@ t_iasn	*asn_primitive_ostring(const char *key, const char *name)
 	return (item);
 }
 
+t_iasn	*asn_primitive_bool(const char *key, const char *name)
+{
+	return (asn_primitive_ostring(key, name));
+}
+
+t_iasn	*asn_primitive_int(const char *key, const char *name)
+{
+	return (asn_primitive_ostring(key, name));
+}
+
 t_iasn	*asn_primitive_bitstring(const char *key, const char *name)
 {
-	t_iasn	*item;
-
-	item = asn_item_init();
-	item->type = ft_strdup(key);
-	return (item);
+	return (asn_primitive_ostring(key, name));
 }
 
 t_iasn	*asn_primitive_null(const char *key, const char *name)
 {
-	t_iasn	*item;
-
-	item = asn_item_init();
-	item->type = ft_strdup(key);
-	return (item);
+	return (asn_primitive_ostring(key, name));
 }
 
 t_iasn	*asn_primitive_oid(const char *key, const char *name)
