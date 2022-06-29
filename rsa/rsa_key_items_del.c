67c87f9077b3d5b35ccc2a99fdab8f3d64b69aa2
nugliar <hashan.hall@gmail.com>
2022-06-29 18:37:51 -0500

    add rsa key item deletion function

---
+++ rsa/rsa_key_items_del.c
@@ -0,0 +1,17 @@
+#include <ft_ssl.h>
+#include <ssl_rsa.h>
+
+int	rsa_key_items_del(t_rsa *rsa_key)
+{
+	key->tflag = 0;
+	del_num(key->version);
+	del_num(key->modulus);
+	del_num(key->pubexp);
+	del_num(key->privexp);
+	del_num(key->prime1);
+	del_num(key->prime2);
+	del_num(key->exponent1);
+	del_num(key->exponent2);
+	del_num(key->coeff);
+	del_num(key->keysize);
+}
