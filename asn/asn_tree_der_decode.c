01a5ab8f72a73aa3773fa77c2ef6eef9aae56c43
nugliar <hashan.hall@gmail.com>
2022-06-30 12:14:06 -0500

    change asn der decode function

--- asn/asn_tree_der_decode.c
+++ asn/asn_tree_der_decode.c
@@ -3,81 +3,97 @@
 #include <ssl_asn.h>
 #include <ssl_der.h>
 
+static t_htbl	*__init_func_htable(void);
+static int		__decode_der_encoding(t_der *, t_node *);
+static int		__decode_recur(t_node *, unsigned char *, size_t , t_htbl *);
+static int		__decode_item(t_iasn *, unsigned char **, size_t *, t_htbl *);
+
 static const int	ASN_TAG	 		= (ASN_TAG_UNIVERSAL | ASN_TAG_SIMPLE);
 static const int	ASN_PRIMITIVE	= (ASN_ENCODE_PRIMITIVE | ASN_TAG);
 static const int	ASN_CONSTRUCT	= (ASN_ENCODE_CONSTRUCT | ASN_TAG);
 
 static const struct {
 	unsigned char	type;
-	int		(*f_read)(t_iasn *, char **, size_t *);
+	int		(*f_read)(t_iasn *, unsigned char *, size_t);
 } T[] = {
-	{	(ASN_CONSTRUCT | ASN_TAG_SEQUENCE),		der_read_sequence	},
-	{	(ASN_PRIMITIVE | ASN_TAG_BOOLEAN),		der_read_bool		},
-	{	(ASN_PRIMITIVE | ASN_TAG_OBJECT_ID),	der_read_oid		},
-	{	(ASN_PRIMITIVE | ASN_TAG_INT),			der_read_int		},
-	{	(ASN_PRIMITIVE | ASN_TAG_OCTET_STRING),	der_read_ostring	},
-	{	(ASN_PRIMITIVE | ASN_TAG_BIT_STRING),	der_read_bitstring	},
-	{	(ASN_PRIMITIVE | ASN_TAG_NULL),			der_read_null		}
+	{	(ASN_CONSTRUCT | ASN_TAG_SEQUENCE),		der_read_sequence_octets	},
+	{	(ASN_PRIMITIVE | ASN_TAG_BOOLEAN),		der_read_bool_octets		},
+	{	(ASN_PRIMITIVE | ASN_TAG_OBJECT_ID),	der_read_oid_octets			},
+	{	(ASN_PRIMITIVE | ASN_TAG_INT),			der_read_int_octets			},
+	{	(ASN_PRIMITIVE | ASN_TAG_OCTET_STRING),	der_read_ostring_octets		},
+	{	(ASN_PRIMITIVE | ASN_TAG_BIT_STRING),	der_read_bitstring_octets	},
+	{	(ASN_PRIMITIVE | ASN_TAG_NULL),			der_read_null_octets		}
 };
+
+int	asn_tree_der_decode(t_der *der, const char *map, t_node **tree)
+{
+	int	ret;
+
+	if (NULL == der || NULL == map || NULL == tree)
+		return (ASN_ERROR(INVALID_INPUT));
+
+	*tree = asn_tree(map);
+
+	ret = __decode_der_encoding(der, *tree);
+
+	if (ret == SSL_OK)
+		return (SSL_OK);
+
+	asn_tree_del(*tree);
+	*tree = NULL;
+
+	return (ASN_ERROR(INVALID_DER_ENCODING));
+}
 
 static t_htbl	*__init_func_htable(void)
 {
 	t_htbl	*htbl;
 
 	if (NULL == (htbl = ft_htbl_init(sizeof(T)/sizeof(*T))))
-	{
 		return (NULL);
-	}
+
 	for (int idx = 0; idx < sizeof(T)/sizeof(*T); idx++)
-	{
 		ft_htbl_add(
 			htbl, T[idx].f_read, &(T[idx].type), sizeof(T[idx].type));
-	}
+
 	return (htbl);
 }
 
-static int	__decode_item(
-	t_iasn *item, char **derenc, size_t *dersize, t_htbl *func_htable)
+static int	__decode_der_encoding(t_der *der, t_node *tree)
 {
-	int	(*f_read)(t_iasn *, void *, size_t *);
+	t_htbl			*func_htable;
+	unsigned char	*derenc;
+	size_t			dersize;
+	int				ret;
 
-	SSL_CHECK((NULL != derenc) && (NULL != *derenc));
-	SSL_CHECK(NULL != dersize);
+	func_htable = __init_func_htable();
 
-	if (*dersize == 0)
-	{
-		return (UNSPECIFIED_ERROR);
-	}
-	if (NULL == (f_read = ft_htbl_get(func_htable, *derenc, 1)))
-	{
-		return (UNSPECIFIED_ERROR);
-	}
-	if (SSL_OK != f_read(item, derenc, dersize))
-	{
-		return (UNSPECIFIED_ERROR);
-	}
-	return (SSL_OK);
+	derenc = (unsigned char *)der->content;
+	dersize = der->size;
+
+	ret = __decode_recur(tree, derenc, dersize, func_htable);
+
+	ft_htbl_del(func_htable);
+
+	return (ret);
 }
 
 static	int __decode_recur(
-	t_node *node, char *derenc, size_t dersize, t_htbl *func_htable)
+	t_node *node, unsigned char *derenc, size_t dersize, t_htbl *func_htable)
 {
-	void	(*f_dec)(t_der *, void *, size_t);
-	t_iasn	*item;
-	char	*child_derenc;
-	size_t	child_derenc_size;
-	int		ret;
+	t_iasn			*item;
+	unsigned char	*child_derenc;
+	size_t			child_derenc_size;
+	int				ret;
 
 	if (NULL == node)
-	{
 		return (SSL_OK);
-	}
+
 	item = node->content;
 
 	if (SSL_OK != __decode_item(item, &derenc, &dersize, func_htable))
-	{
 		return (SSL_ERROR(INVALID_DER_ENCODING));
-	}
+
 	if (ft_node_is_parent(node))
 	{
 		child_derenc = item->content;
@@ -91,34 +107,49 @@ static	int __decode_recur(
 		item->bitsize = 0;
 
 		if (SSL_OK != ret)
-		{
 			return (SSL_ERROR(INVALID_DER_ENCODING));
-		}
 	}
 	return (__decode_recur(node->next, derenc, dersize, func_htable));
 }
 
-int	asn_tree_der_decode(t_der *der, const char *map, t_node **tree)
+static int	__decode_item(
+	t_iasn *item, unsigned char **derenc, size_t *dersize, t_htbl *func_htable)
 {
-	t_htbl	*func_htable;
-	int		ret;
+	int		(*f_read)(t_iasn *, unsigned char *, size_t);
+	size_t	readsize;
+	size_t	lensize;
+	size_t	len;
 
-	SSL_CHECK(NULL != tree);
-	SSL_CHECK(NULL != der);
-	SSL_CHECK(NULL != map);
+	readsize = 0;
 
-	ret = SSL_OK;
+	if (NULL == item || NULL == derenc || NULL == dersize)
+		return (ASN_ERROR(INVALID_INPUT));
 
-	*tree = asn_tree(map);
-	func_htable = __init_func_htable();
+	if (*dersize == 0)
+		return (UNSPECIFIED_ERROR);
 
-	if (SSL_OK != (ret = __decode_recur(
-		*tree, der->content, der->size, func_htable)))
-	{
-		asn_tree_del(*tree);
-		*tree = NULL;
-	}
-	ft_htbl_del(func_htable);
+	if (NULL == (f_read = ft_htbl_get(func_htable, *derenc, 1)))
+		return (UNSPECIFIED_ERROR);
 
-	return (ret);
+	readsize++;
+
+	if (SSL_OK != der_read_len_size(&lensize, *derenc, *dersize))
+		return (UNSPECIFIED_ERROR);
+
+	readsize++;
+
+	if (SSL_OK != der_read_len_octets(&len, *derenc, lensize))
+		return (UNSPECIFIED_ERROR);
+
+	readsize += lensize;
+
+	if (SSL_OK != f_read(item, *derenc, len))
+		return (UNSPECIFIED_ERROR);
+
+	readsize += len;
+
+	*derenc += readsize;
+	*dersize -= readsize;
+
+	return (SSL_OK);
 }
