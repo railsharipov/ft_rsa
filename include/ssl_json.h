75820171e35c174583fe873a862b157fa698c156
Rail Sharipov <rail.sharipov@node40.com>
2024-01-08 21:45:46 -0600

    add json parse functions

--- include/ssl_json.h
+++ include/ssl_json.h
@@ -1,11 +1,21 @@
 #ifndef SSL_JSON_H
 # define SSL_JSON_H
 
+enum e_json_error
+{
+	INVALID_JSON_TYPE = 1,
+	PARSE_JSON_FAILED,
+	INVALID_FORMAT,
+	UNEXPECTED_END_OF_CONTENT,
+	UNEXPECTED_CHARS_AT_THE_END,
+};
+
 enum e_json_type{
     JSON_ARRAY,
     JSON_OBJECT,
-    JSON_STRING,
-    JSON_INTEGER
+    JSON_CSTR,
+    JSON_NUMBER,
+    JSON_NULL,
 };
 
 int     json_parse(const char *s, t_node **node);
--- json/json_parse.c
+++ json/json_parse.c
@@ -1,127 +1,381 @@
-// #include <ft_ssl.h>
-// #include <ssl_json.h>
-// #include <ssl_map.h>
-// #include <bnum.h>
+#include <ft_ssl.h>
+#include <ssl_json.h>
+#include <ssl_map.h>
+#include <bnum.h>
 
-// typedef ssize_t (*FUNC_JSON_PARSE)(const char *, t_node **);
+typedef ssize_t (*FUNC_JSON_PARSE)(const char *, t_node **);
 
-// t_htbl  *__init_htable(void);
+static t_htbl	*__htable;
 
-// ssize_t __parse(const char *, t_node **, t_htbl *);
-// ssize_t __parse_array(const char *, t_node **);
-// ssize_t __parse_object(const char *, t_node **);
-// ssize_t __parse_string(const char *, t_node **);
-// ssize_t __parse_integer(const char *, t_node **);
+int		__init_htable(void);
+int		__is_ws_only(const char *);
 
-// static const struct
-// {
-//     char    symbol;
-//     FUNC_JSON_PARSE f;
-// } T[] = {
-//     {'{',   __parse_object},
-//     {'[',   __parse_array},
-//     {'"',   __parse_string},
-//     {'\'',   __parse_string}
-// };
+ssize_t __parse(const char *, t_node *);
+ssize_t __parse_array(const char *, t_node *);
+ssize_t __parse_object(const char *, t_node *);
+ssize_t __parse_string(const char *, t_node *);
+ssize_t __parse_number(const char *, t_node *);
+ssize_t __null(const char *, t_node *);
 
-// int json_parse(const char *s, t_node **node)
-// {
-//     t_htbl  *ht;
+void	__delete(t_node *);
+void	__delete_array(t_node *);
+void	__delete_object(t_node *);
+void	__delete_string(t_node *);
+void	__delete_number(t_node *);
 
-//     if (NULL == s || NULL == node)
-//         return SSL_ERROR(UNSPECIFIED_ERROR);
+typedef struct		s_json_ctx
+{
+	FUNC_JSON_PARSE f;
+	char 			open;
+	char 			close;
+}					t_json_ctx;
 
-//     *node = NULL;
+static const t_json_ctx T[] = {
+    {	__parse,		'{', 	'}',   	},
+    {	__parse_array,	'[', 	']',   	},
+    {	__parse_string,	'"', 	'"',   	},
+    {	__parse_string,	'\'', 	'\'',	},
+    {	__null,			'\0', 	'\0',	},
+};
 
-//     ht = __init_htable();
+int json_parse(const char *s, t_node **node)
+{
+	t_node 	*json_node;
+	t_node 	end_node;
+	ssize_t	rbytes;
 
-//     if (NULL == ht)
-//         return SSL_ERROR(UNSPECIFIED_ERROR);
+	if (NULL == s || NULL == node) {
+        return SSL_ERROR(INVALID_INPUT);
+	}
 
-//     if (SSL_OK != __parse(s, node, ht))
-//         return SSL_ERROR(UNSPECIFIED_ERROR);
+    *node = NULL;
 
-//     return SSL_OK;
+	if (SSL_OK != __init_htable()) {
+		return SSL_ERROR(UNSPECIFIED_ERROR);
+	}
+	json_node = ft_node_create();
+	rbytes = __parse(s, json_node);
 
-// err:
-//     ft_htbl_del(ht);
-//     return SSL_ERROR(UNSPECIFIED_ERROR);
-// }
+	if (rbytes < 0) {
+		__delete(json_node);
+		return SSL_ERROR(PARSE_JSON_FAILED);
+	}
+	if (!__is_ws_only(s + rbytes)) {
+		__delete(json_node);
+		return SSL_ERROR(UNEXPECTED_CHARS_AT_THE_END);
+	}
+	*node = json_node;
+    ft_htbl_del(__htable);
 
-// t_htbl  *__init_htable(void)
-// {
-//     t_htbl  *htbl;
-//     int idx;
-//     size_t ht_size;
+    return SSL_OK;
+}
 
-//     ht_size = sizeof(T)/sizeof(T[0]);
+int	__init_htable(void)
+{
+    int idx;
+    size_t ht_size;
 
-//     if (NULL == (htbl = ft_htbl_init(ht_size)))
-//         return NULL;
-    
-//     idx = 0;
-//     while (idx < ht_size)
-//     {
-//         ft_htbl_bin_add(htbl, (void *)&(T[idx]), &(T[idx].symbol), sizeof(T[idx].symbol));
-//         idx++;
-//     }
+    ht_size = sizeof(T)/sizeof(T[0]);
 
-//     return htbl;
-// }
+	if (NULL == (__htable = ft_htbl_init(ht_size))) {
+		return (SSL_ERROR(MEMORY_ERROR));
+	}
 
-// ssize_t __parse(const char *s, t_node **node, t_htbl *ht)
-// {
-//     FUNC_JSON_PARSE f_parse;
-//     t_node  *cur_node;
-//     ssize_t rbytes;
-//     ssize_t idx;
+	idx = 0;
+    while (idx < ht_size) {
+		ft_htbl_bin_add(__htable, (void *)&(T[idx]), &(T[idx].open), sizeof(T[idx].open));
+		idx++;
+    }
+	return (SSL_OK);
+}
 
-//     idx = 0;
-//     cur_node = NULL;
+int	__is_ws_only(const char *s)
+{
+	while (ft_iseolws(*s)) {
+		s++;
+	}
+	if (*s == '\0') {
+		return (1);
+	} else {
+		return (0);
+	}
+}
 
-//     while (s[idx] != '\0') {
-//         while (ft_iseolws(s[idx]))
-//             idx++;
-        
-//         f_parse = ft_htbl_bin_get(ht, s, sizeof(char));
+ssize_t __parse(const char *s, t_node *node)
+{
+	FUNC_JSON_PARSE	f_parse;
+	t_json_ctx 		*ctx;
+    ssize_t 		rbytes;
+    ssize_t 		idx;
+	char			open, close;
 
-//         if (NULL == f_parse)
-//             f_parse = __parse_integer;
+    idx = 0;
 
-//         rbytes = f_parse(s + idx, &cur_node);
+	while (ft_iseolws(s[idx])) {
+		idx++;
+	}
+	open = s[idx];
+	ctx = ft_htbl_bin_get(__htable, open, sizeof(open));
 
-//         if (rbytes <= 0)
-//             return SSL_ERROR(UNSPECIFIED_ERROR);
+	if (NULL == ctx) {
+		f_parse = __parse_number;
+		open = 0;
+		close = 0;
+	} else {
+		f_parse = ctx->f;
+		close = ctx->close;
+	}
 
-//         idx += rbytes;
+	if (open != 0) {
+		idx++;
+	}
+	while (ft_iseolws(s[idx])) {
+		idx++;
+	}
 
-//         ft_lst_append(node, cur_node);
-//     }
+	rbytes = f_parse(s + idx, node);
 
-//     return SSL_OK;
-// }
+	if (rbytes <= 0) {
+		SSL_ERROR(UNSPECIFIED_ERROR);
+		return (-1);
+	} else {
+		idx += rbytes;
+	}
 
-// ssize_t __parse_integer(const char *s, t_node **node)
-// {
-//     t_node      *node;
-//     t_num       *num;
-//     ssize_t     idx;
-//     ssize_t     integer;
-//     char        *sub_s;
+	if (close) {
+		while (ft_iseolws(s[idx])) {
+			idx++;
+		}
+		if (s[idx] != close) {
+			SSL_ERROR(UNEXPECTED_END_OF_CONTENT);
+			return (-1);
+		} else {
+			idx++;
+		}
+	}
 
-//     idx = 0;
-//     while (s[idx] == '-') {
-//         idx++;
-//     }
-//     while (s[idx] != '\0' && ft_isdigit(s[idx])) {
-//         idx++;
-//     }
-//     sub_s = ft_strsub(s, 0, idx);
-//     integer = ft_atoi(sub_s);
-//     SSL_FREE(sub_s);
-//     value = ft_memdup(integer, sizeof(integer));
+	return (idx);
+}
 
-//     node = ft_node_new(NULL, &value, sizeof(value));
+ssize_t __parse_number(const char *s, t_node *node)
+{
+    t_num   *num;
+    ssize_t	idx;
+    char    *sub_s;
 
-//     return ();
-// }
+    idx = 0;
+    if (s[idx] == '-') {
+        idx++;
+    }
+	if (!ft_isdigit(s[idx])) {
+		SSL_ERROR(INVALID_FORMAT);
+		return (-1);
+	}
+    while (ft_isdigit(s[idx])) {
+		idx++;
+    }
+    sub_s = ft_strsub(s, 0, idx);
+    num = bnum_create();
+	bnum_init(num);
+	bnum_from_dec(num, sub_s);
+
+	node->content = num;
+	node->size = sizeof(t_num);
+	node->type = JSON_NUMBER;
+	node->f_del = __delete_number;
+
+	SSL_FREE(sub_s);
+
+    return (idx);
+}
+
+ssize_t __parse_string(const char *s, t_node *node)
+{
+	ssize_t idx;
+
+	idx = 0;
+	while (s[idx] != '\0' && s[idx] != '\'' && s[idx] != '"') {
+		idx++;
+	}
+	node->content = ft_strsub(s, 0, idx);
+	node->size = (size_t)idx;
+	node->type = JSON_CSTR;
+	node->f_del = __delete_string;
+
+	return (idx);
+}
+
+ssize_t __parse_object(const char *s, t_node *node)
+{
+	t_node	*key_node;
+	t_node	*content_node;
+	t_htbl	*htbl;
+	ssize_t	rbytes;
+	ssize_t idx;
+	int		next;
+
+	htbl = ft_htbl_init(0);
+	idx = 0;
+
+	do {
+		key_node = ft_node_create();
+		rbytes = __parse(s + idx, key_node);
+
+		if (rbytes < 0) {
+			SSL_ERROR(INVALID_FORMAT);
+			goto err;
+		}
+		idx += rbytes;
+
+		if (key_node->type != JSON_CSTR) {
+			SSL_ERROR(INVALID_FORMAT);
+			goto err;
+		}
+
+		while (ft_iseolws(s[idx])) {
+			idx++;
+		}
+		if (s[idx] != ':') {
+			SSL_ERROR(INVALID_FORMAT);
+			goto err;
+		} else {
+			idx++;
+		}
+
+		content_node = ft_node_create();
+		rbytes = __parse(s + idx, content_node);
+
+		if (rbytes < 0) {
+			SSL_ERROR(INVALID_FORMAT);
+			goto err;
+		}
+		idx += rbytes;
+
+		ft_htbl_add(htbl, content_node, (const char *)key_node->content);
+		ft_node_del(key_node);
+
+		while (ft_iseolws(s[idx])) {
+			idx++;
+		}
+		if (s[idx] == ',') {
+			next = SSL_TRUE;
+			idx++;
+		} else {
+			next = SSL_FALSE;
+		}
+	} while (next);
+
+	node->content = htbl;
+	node->size = sizeof(t_htbl);
+	node->type = JSON_OBJECT;
+	node->f_del = __delete_object;
+
+	return (idx);
+err:
+	ft_node_del(key_node);
+	ft_node_del(content_node);
+	ft_htbl_del(htbl);
+	return (-1);
+}
+
+ssize_t __parse_array(const char *s, t_node *node)
+{
+	t_node	*list;
+	t_node	*content_node;
+	ssize_t	rbytes;
+	ssize_t idx;
+	int		next;
+
+	list = NULL;
+	idx = 0;
+
+	do {
+		while (ft_iseolws(s[idx])) {
+			idx++;
+		}
+
+		content_node = ft_node_create();
+		rbytes = __parse(s + idx, content_node);
+
+		if (rbytes < 0) {
+			SSL_ERROR(INVALID_FORMAT);
+			goto err;
+		}
+		idx += rbytes;
+
+		ft_lst_prepend(&list, content_node);
+
+		while (ft_iseolws(s[idx])) {
+			idx++;
+		}
+		if (s[idx] == ',') {
+			next = SSL_TRUE;
+			idx++;
+		} else {
+			next = SSL_FALSE;
+		}
+	} while (next);
+
+	node->content = list;
+	node->size = sizeof(t_node);
+	node->type = JSON_ARRAY;
+	node->f_del = __delete_array;
+
+	return (idx);
+err:
+	ft_node_del(content_node);
+	ft_lst_del(list, NULL);
+	return (-1);
+}
+
+ssize_t __null(const char *s, t_node *node)
+{
+	node->content = NULL;
+	node->size = 0;
+	node->type = JSON_NULL;
+
+	return (0);
+}
+
+void	__delete(t_node *node) {
+	if (node->type == JSON_CSTR) {
+		__delete_string(node);
+	} else if (node->type == JSON_NUMBER) {
+		__delete_number(node);
+	} else if (node->type == JSON_OBJECT) {
+		__delete_object(node);
+	} else if (node->type == JSON_ARRAY) {
+		__delete_array(node);
+	} else {
+		LIBFT_FREE(node->key);
+		LIBFT_FREE(node);
+	}
+}
+
+void	__delete_array(t_node *node)
+{
+	ft_lst_del((t_node *)(node->content), __delete);
+	LIBFT_FREE(node->key);
+	LIBFT_FREE(node);
+}
+
+void	__delete_object(t_node *node)
+{
+	ft_htbl_del((t_htbl *)(node->content));
+	LIBFT_FREE(node->key);
+	LIBFT_FREE(node);
+}
+
+void	__delete_string(t_node *node)
+{
+	LIBFT_FREE(node->content);
+	LIBFT_FREE(node->key);
+	LIBFT_FREE(node);
+}
+
+void	__delete_number(t_node *node)
+{
+	bnum_del((t_num *)(node->content));
+	LIBFT_FREE(node->key);
+	LIBFT_FREE(node);
+}
