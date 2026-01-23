2b98973eca3437cf0c01679c013350cf3670a8b7
Rail Sharipov <rail.sharipov@node40.com>
2026-01-23 12:53:29 -0700

    update tests

--- test/test_args.c
+++ test/test_args.c
@@ -38,20 +38,20 @@ static int	__test_args_parse(void)
 	// cmd = args_cmd_new("test", NULL, NULL);
 	// TEST_ASSERT(cmd != NULL);
 
-	// opt = args_opt_new("-v", "test", AP_ARG_TYPE_STRING);
+	// opt = args_opt_new("-v", "test", AP_OPT_TYPE_STRING);
 	// TEST_ASSERT(opt != NULL);
-	// ret = args_add_cmd_opt(cmd, opt);
+	// ret = args_add_sub_cmd(cmd, opt);
 	// TEST_ASSERT(ret == SSL_OK);
-	// opt = args_opt_new("-h", "test", AP_ARG_TYPE_NUMBER);
+	// opt = args_opt_new("-h", "test", AP_OPT_TYPE_NUMBER);
 	// TEST_ASSERT(opt != NULL);
-	// ret = args_add_cmd_opt(cmd, opt);
+	// ret = args_add_sub_cmd(cmd, opt);
 	// TEST_ASSERT(ret == SSL_OK);
-	// opt = args_opt_new("-i", "test", AP_ARG_TYPE_FLAG);
+	// opt = args_opt_new("-i", "test", AP_OPT_TYPE_FLAG);
 	// TEST_ASSERT(opt != NULL);
-	// ret = args_add_cmd_opt(cmd, opt);
+	// ret = args_add_sub_cmd(cmd, opt);
 	// TEST_ASSERT(ret == SSL_OK);
 
-	// ret = args_add_cmd(&args, cmd);
+	// ret = args_add_sub_cmd(&args, cmd);
 	// TEST_ASSERT(ret == SSL_OK);
 
 	// ret = args_parse(&args, sargs);
@@ -66,20 +66,20 @@ static int	__test_args_parse(void)
 	// TEST_ASSERT(opt != NULL);
 	// TEST_ASSERT(opt->set == 1);
 	// TEST_ASSERT(opt->pos == 1);
-	// TEST_ASSERT(opt->type == AP_ARG_TYPE_STRING);
+	// TEST_ASSERT(opt->type == AP_OPT_TYPE_STRING);
 	// TEST_ASSERT(ft_strcmp(opt->value, "all") == 0);
 
 	// opt = args_get_cmd_opt(cmd, "-h");
 	// TEST_ASSERT(opt != NULL);
 	// TEST_ASSERT(opt->set == 1);
 	// TEST_ASSERT(opt->pos == 3);
-	// TEST_ASSERT(opt->type == AP_ARG_TYPE_NUMBER);
+	// TEST_ASSERT(opt->type == AP_OPT_TYPE_NUMBER);
 	// TEST_ASSERT(ft_strcmp(opt->value, "10") == 0);
 
 	// opt = args_get_cmd_opt(cmd, "-i");
 	// TEST_ASSERT(opt != NULL);
 	// TEST_ASSERT(opt->set == 0);
-	// TEST_ASSERT(opt->type == AP_ARG_TYPE_FLAG);
+	// TEST_ASSERT(opt->type == AP_OPT_TYPE_FLAG);
 
 	// // test 2
 	// ret = args_init(&args);
@@ -88,20 +88,20 @@ static int	__test_args_parse(void)
 	// cmd = args_cmd_new("test", NULL, NULL);
 	// TEST_ASSERT(cmd != NULL);
 
-	// opt = args_opt_new("-v", "test", AP_ARG_TYPE_STRING);
+	// opt = args_opt_new("-v", "test", AP_OPT_TYPE_STRING);
 	// TEST_ASSERT(opt != NULL);
-	// ret = args_add_cmd_opt(cmd, opt);
+	// ret = args_add_sub_cmd(cmd, opt);
 	// TEST_ASSERT(ret == SSL_OK);
-	// opt = args_opt_new("-h", "test", AP_ARG_TYPE_NUMBER);
+	// opt = args_opt_new("-h", "test", AP_OPT_TYPE_NUMBER);
 	// TEST_ASSERT(opt != NULL);
-	// ret = args_add_cmd_opt(cmd, opt);
+	// ret = args_add_sub_cmd(cmd, opt);
 	// TEST_ASSERT(ret == SSL_OK);
-	// opt = args_opt_new("-i", "test", AP_ARG_TYPE_FLAG);
+	// opt = args_opt_new("-i", "test", AP_OPT_TYPE_FLAG);
 	// TEST_ASSERT(opt != NULL);
-	// ret = args_add_cmd_opt(cmd, opt);
+	// ret = args_add_sub_cmd(cmd, opt);
 	// TEST_ASSERT(ret == SSL_OK);
 
-	// ret = args_add_cmd(&args, cmd);
+	// ret = args_add_sub_cmd(&args, cmd);
 	// TEST_ASSERT(ret == SSL_OK);
 
 	// ret = args_parse(&args, sargs_2);
@@ -116,19 +116,19 @@ static int	__test_args_parse(void)
 	// TEST_ASSERT(opt != NULL);
 	// TEST_ASSERT(opt->set == 1);
 	// TEST_ASSERT(opt->pos == 3);
-	// TEST_ASSERT(opt->type == AP_ARG_TYPE_FLAG);
+	// TEST_ASSERT(opt->type == AP_OPT_TYPE_FLAG);
 
 	// opt = args_get_cmd_opt(cmd, "-v");
 	// TEST_ASSERT(opt != NULL);
 	// TEST_ASSERT(opt->set == 1);
 	// TEST_ASSERT(opt->pos == 1);
-	// TEST_ASSERT(opt->type == AP_ARG_TYPE_STRING);
+	// TEST_ASSERT(opt->type == AP_OPT_TYPE_STRING);
 	// TEST_ASSERT(ft_strcmp(opt->value, "all") == 0);
 
 	// opt = args_get_cmd_opt(cmd, "-h");
 	// TEST_ASSERT(opt != NULL);
 	// TEST_ASSERT(opt->set == 0);
-	// TEST_ASSERT(opt->type == AP_ARG_TYPE_NUMBER);
+	// TEST_ASSERT(opt->type == AP_OPT_TYPE_NUMBER);
 
 	return (SSL_OK);
 }
--- test/test_io.c
+++ test/test_io.c
@@ -1,26 +1,17 @@
 #include <common.h>
 #include <io.h>
+#include <file.h>
 #include <test.h>
-#include <unistd.h>
-#include <fcntl.h>
+#include <libft/string.h>
 
 static int	__test_io_setup(void);
-static int	__test_io_init(void);
-// static int	__test_io_read(void);
-// static int	__test_io_write(void);
+static int	__test_io_file_read(void);
+static int	__test_io_file_write(void);
 
-static const char	*__lorem = "Cernantur iis sunt, voluptate export nulla \
-	arbitror noster. Se nescius exercitation. Malis nescius o consectetur \
-	est excepteur multos dolor excepteur quae id ut aute incididunt \
-	domesticarum eu malis a voluptate ubi ita esse pariatur offendit de \
-	quamquam quid qui commodo cohaerescant si do elit incurreret singulis, \
-	est ita consectetur. Ut veniam constias deserunt, nisi cupidatat est esse \
-	cillum. O id arbitrantur si eiusmod velit nostrud probant sed probant nisi \
-	constias si eram pariatur ubi coniunctione, magna voluptate se probant, \
-	iis eu tamen irure sunt id laborum tempor arbitror, excepteur ad noster. \
-	Laboris enim constias doctrina. Elit nostrud in multos sint.";
+static const char	*__small_text_file_path = "test/files/text/small.txt";
+static const char	*__large_text_file_path = "test/files/text/large.txt";
 
-static size_t	__lorem_size;
+static const char	*__test_text_file_path = "test/files/text/test.txt";
 
 int	test_io(void)
 {
@@ -30,52 +21,179 @@ int	test_io(void)
 	}
 
 	return (
-		__test_io_init()
-		// | __test_io_read()
-		// | __test_io_write()
+		__test_io_file_read()
+		| __test_io_file_write()
 	);
 }
 
 static int	__test_io_setup(void)
 {
-	__lorem_size = ft_strlen(__lorem);
-
 	return (SSL_OK);
 }
 
-static int	__test_io_init(void)
+static int	__test_io_file_read(void)
 {
-	t_ostring	osbuf;
-	t_iodes		iodes;
+	t_io_v2_stream	*stream;
+	t_ostring		ref_content, test_content;
+	char 			*buf;
+	size_t			bufsize;
+	ssize_t			rbytes, tbytes;
+	int				ret;
 
-	io_fopen(&iodes, IO_READ|IO_FILE, "./Makefile");
+	// Small file
+	if (SSL_OK != file_read_all(__small_text_file_path, &ref_content)) {
+		TEST_LOG(ERROR, FILE_READ_ERROR);
+		TEST_FAIL();
+	}
+	bufsize = ref_content.size;
+	SSL_ALLOC(buf, bufsize);
 
-	TEST_ASSERT(iodes.fd > 2);
-	TEST_ASSERT(iodes.mode == IO_MODE_FILDES);
+	ret = file_reader(&stream, __small_text_file_path);
+	TEST_ASSERT(SSL_OK == ret);
+	TEST_ASSERT(stream->status == IO_V2_STATUS_OK);
+	TEST_ASSERT(stream->interface.read != NULL);
+	TEST_ASSERT(stream->interface.write == NULL);
+	TEST_ASSERT(stream->interface.close != NULL);
+	TEST_ASSERT(stream->interface.flush == NULL);
+	TEST_ASSERT(stream->ctx != NULL);
+	TEST_ASSERT(stream->flags == (IO_V2_FLAG_READ | IO_V2_FLAG_CLOSE));
 
-	io_fopen(&iodes, IO_READ|IO_STDIN, NULL);
+	rbytes = io_v2_read(stream, buf, bufsize);
+	TEST_ASSERT(rbytes > 0);
+	TEST_ASSERT(rbytes == bufsize);
+	TEST_ASSERT(ft_memcmp(buf, ref_content.content, bufsize) == 0);
+	TEST_ASSERT(stream->status == IO_V2_STATUS_OK);
 
-	TEST_ASSERT(iodes.fd == STDIN_FILENO);
-	TEST_ASSERT(iodes.mode == IO_MODE_FILDES);
+	rbytes = io_v2_read(stream, buf, bufsize);
+	TEST_ASSERT(rbytes == 0);
+	TEST_ASSERT(stream->status == IO_V2_STATUS_EOF);
 
-	io_fopen(&iodes, IO_WRITE|IO_STDOUT, NULL);
+	rbytes = io_v2_close(stream);
+	TEST_ASSERT(rbytes == 0);
+	TEST_ASSERT(stream->status == IO_V2_STATUS_OK);
 
-	TEST_ASSERT(iodes.fd == STDOUT_FILENO);
-	TEST_ASSERT(iodes.mode == IO_MODE_FILDES);
+	rbytes = io_v2_read(stream, buf, 1);
+	TEST_ASSERT(rbytes == -1);
+	TEST_ASSERT(stream->status == IO_V2_STATUS_ERROR);
 
-	osbuf.content = (unsigned char *)"some_content";
-	osbuf.size = ft_strlen((char *)osbuf.content);
+	ft_ostr_clear(&ref_content);
 
-	io_osbuf(&iodes, IO_READ|IO_OSBUF, &osbuf);
+	// Large file
+	if (SSL_OK != file_read_all(__large_text_file_path, &ref_content)) {
+		TEST_LOG(ERROR, FILE_READ_ERROR);
+		TEST_FAIL();
+	}
+	bufsize = IO_BUFSIZE;
+	SSL_ALLOC(buf, bufsize);
 
-	TEST_ASSERT(iodes.osbuf == &osbuf);
-	TEST_ASSERT(iodes.mode == IO_MODE_OSBUF);
+	ret = file_reader(&stream, __large_text_file_path);
+	TEST_ASSERT(SSL_OK == ret);
 
-	io_osbuf(&iodes, IO_WRITE|IO_OSBUF, &osbuf);
+	ft_ostr_init(&test_content);
+	tbytes = 0;
 
-	TEST_ASSERT(iodes.osbuf == &osbuf);
-	TEST_ASSERT(iodes.mode == IO_MODE_OSBUF);
+	while (stream->status == IO_V2_STATUS_OK) {
+		rbytes = io_v2_read(stream, buf, bufsize);
+		if (rbytes < 0) {
+			break;
+		}
+		ft_ostr_append(&test_content, buf, rbytes);
+		tbytes += rbytes;
+	}
+	TEST_ASSERT(stream->status == IO_V2_STATUS_EOF);
+	TEST_ASSERT(tbytes == ref_content.size);
+	TEST_ASSERT(ft_memcmp(test_content.content, ref_content.content, tbytes) == 0);
+
+	rbytes = io_v2_close(stream);
+	TEST_ASSERT(rbytes == 0);
+	TEST_ASSERT(stream->status == IO_V2_STATUS_OK);
+
+	ft_ostr_clear(&test_content);
+	ft_ostr_clear(&ref_content);
+	SSL_FREE(buf);
 
 	TEST_PASS();
 }
 
+static int	__test_io_file_write(void)
+{
+	t_io_v2_stream	*stream;
+	t_ostring		ref_content, test_content;
+	ssize_t			wbytes, tbytes;
+	int				ret;
+
+	// Small file
+	if (SSL_OK != file_read_all(__small_text_file_path, &ref_content)) {
+		TEST_LOG(ERROR, FILE_READ_ERROR);
+		TEST_FAIL();
+	}
+
+	ret = file_writer(&stream, __test_text_file_path);
+	TEST_ASSERT(SSL_OK == ret);
+	TEST_ASSERT(stream->status == IO_V2_STATUS_OK);
+	TEST_ASSERT(stream->interface.read == NULL);
+	TEST_ASSERT(stream->interface.write != NULL);
+	TEST_ASSERT(stream->interface.close != NULL);
+	TEST_ASSERT(stream->interface.flush == NULL);
+	TEST_ASSERT(stream->ctx != NULL);
+	TEST_ASSERT(stream->flags == (IO_V2_FLAG_WRITE | IO_V2_FLAG_CLOSE));
+
+	wbytes = io_v2_write(stream, ref_content.content, ref_content.size);
+	TEST_ASSERT(wbytes > 0);
+	TEST_ASSERT(wbytes == ref_content.size);
+	TEST_ASSERT(stream->status == IO_V2_STATUS_OK);
+
+	wbytes = io_v2_close(stream);
+	TEST_ASSERT(wbytes == 0);
+	TEST_ASSERT(stream->status == IO_V2_STATUS_OK);
+
+	if (SSL_OK != file_read_all(__test_text_file_path, &test_content)) {
+		TEST_LOG(ERROR, FILE_READ_ERROR);
+		TEST_FAIL();
+	}
+	TEST_ASSERT(test_content.size == ref_content.size);
+	TEST_ASSERT(ft_memcmp(test_content.content, ref_content.content, ref_content.size) == 0);
+
+	ft_ostr_clear(&ref_content);
+	ft_ostr_clear(&test_content);
+
+	// Large file
+	if (SSL_OK != file_read_all(__large_text_file_path, &ref_content)) {
+		TEST_LOG(ERROR, FILE_READ_ERROR);
+		TEST_FAIL();
+	}
+
+	ret = file_writer(&stream, __large_text_file_path);
+	TEST_ASSERT(SSL_OK == ret);
+
+	tbytes = 0;
+
+	while (tbytes < ref_content.size && stream->status == IO_V2_STATUS_OK) {
+		wbytes = io_v2_write(stream, ref_content.content + tbytes, 512);
+		if (wbytes < 0) {
+			break;
+		}
+		if (wbytes == 0) {
+			break;
+		}
+		tbytes += wbytes;
+	}
+	TEST_ASSERT(stream->status == IO_V2_STATUS_OK);
+	TEST_ASSERT(tbytes == ref_content.size);
+
+	wbytes = io_v2_close(stream);
+	TEST_ASSERT(wbytes == 0);
+	TEST_ASSERT(stream->status == IO_V2_STATUS_OK);
+
+	if (SSL_OK != file_read_all(__test_text_file_path, &test_content)) {
+		TEST_LOG(ERROR, FILE_READ_ERROR);
+		TEST_FAIL();
+	}
+	TEST_ASSERT(test_content.size == ref_content.size);
+	TEST_ASSERT(ft_memcmp(test_content.content, ref_content.content, ref_content.size) == 0);
+
+	ft_ostr_clear(&ref_content);
+	ft_ostr_clear(&test_content);
+
+	TEST_PASS();
+}
