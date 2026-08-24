525d3f3d0008314dfbfc28df49d15d4a11ef9cab
Rail Sharipov <hashan.hall@gmail.com>
2026-08-24 17:37:47 -0600

    init unimplemeted commands

--- cmd/cmd_all.c
+++ cmd/cmd_all.c
@@ -5,30 +5,36 @@
 
 int	cmd_hash(const t_cmd *cmd)
 {
-	return (SSL_OK);
+	SSL_LOG(ERROR, NOT_IMPLEMENTED_ERROR);
+	return (SSL_ERR);
 }
 
 int	cmd_des_ecb(const t_cmd *cmd)
 {
-	return (SSL_OK);
+	SSL_LOG(ERROR, NOT_IMPLEMENTED_ERROR);
+	return (SSL_ERR);
 }
 
 int	cmd_des_cbc(const t_cmd *cmd)
 {
-	return (SSL_OK);
+	SSL_LOG(ERROR, NOT_IMPLEMENTED_ERROR);
+	return (SSL_ERR);
 }
 
 int	cmd_rsa_gen(const t_cmd *cmd)
 {
-	return (SSL_OK);
+	SSL_LOG(ERROR, NOT_IMPLEMENTED_ERROR);
+	return (SSL_ERR);
 }
 
 int	cmd_rsa(const t_cmd *cmd)
 {
-	return (SSL_OK);
+	SSL_LOG(ERROR, NOT_IMPLEMENTED_ERROR);
+	return (SSL_ERR);
 }
 
 int	cmd_rsa_utl(const t_cmd *cmd)
 {
-	return (SSL_OK);
+	SSL_LOG(ERROR, NOT_IMPLEMENTED_ERROR);
+	return (SSL_ERR);
 }
