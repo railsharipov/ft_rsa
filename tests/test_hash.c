a49957f61f82ce06c045a7b2f46464d3afcba1d4
Rail Sharipov <hashan.hall@gmail.com>
2026-08-25 16:37:31 -0600

    update digest/hash tests

--- tests/test_hash.c
+++ tests/test_hash.c
@@ -1,27 +1,34 @@
 #include <common.h>
 #include <logger.h>
 #include <hash.h>
-#include "test.h"
 #include <io.h>
+#include <file.h>
 #include <libft.h>
+
+#include "test.h"
 
 static int	__test_hash_setup(void);
 
 static int	__test_hash_md5(void);
+static int	__test_hash_md5_transform(void);
+
 static int	__test_hash_sha1(void);
-static int	__test_hash_sha224(void);
+static int	__test_hash_sha1_transform(void);
+
 static int	__test_hash_sha256(void);
-static int	__test_hash_sha384(void);
+static int	__test_hash_sha256_transform(void);
+static int	__test_hash_sha224_transform(void);
+
 static int	__test_hash_sha512(void);
+static int	__test_hash_sha512_transform(void);
+static int	__test_hash_sha384_transform(void);
 
 static const char	*__small_text_file_path = "tests/files/text/small.txt";
 static const char	*__large_text_file_path = "tests/files/text/large.txt";
 
 static const char	*__small_text_md5_hash_hex = "41ea606938d6dfdeab2ac96a08114598";
 static const char	*__small_text_sha1_hash_hex = "2d3302cfd46bedd06600433e04325c9779a30710";
-static const char	*__small_text_sha224_hash_hex = "0d549e38d356a88a2fbfe54976bf3f2d27cf7ab83608cb7a7259c69f";
 static const char	*__small_text_sha256_hash_hex = "e3cc9b1cbd3c9a0cea76629b3951fd412df5dc8810f47ef498a76f30d6d8ca18";
-static const char	*__small_text_sha384_hash_hex = "6c25956f00aff98b52ddfc84deebc33056700008379b4df4f54cabc10fe0738f47a4b9368412ce0d044e00be97c1795c";
 static const char	*__small_text_sha512_hash_hex = "2c38c8a19dddbc1c3187e54ceb55a0634db2f541bc11c2db2970e1cb8f37be450d5be343dcaba9db2d7372832f399841d7fbf40c5acc1ea81237303a1891521c";
 
 static const char	*__large_text_md5_hash_hex = "54500f5736bb406b1414a38997bf4b9b";
@@ -38,12 +45,17 @@ int	test_hash(void)
 		TEST_FAIL();
 	}
 
-    return (__test_hash_md5()
+	return __test_hash_md5()
+		| __test_hash_md5_transform()
         | __test_hash_sha1()
-        | __test_hash_sha224()
+		| __test_hash_sha1_transform()
         | __test_hash_sha256()
-        | __test_hash_sha384()
-        | __test_hash_sha512());
+        | __test_hash_sha256_transform()
+        | __test_hash_sha224_transform()
+        | __test_hash_sha512()
+        | __test_hash_sha512_transform()
+        | __test_hash_sha384_transform()
+    	;
 }
 
 static int	__test_hash_setup(void)
@@ -53,186 +65,452 @@ static int	__test_hash_setup(void)
 
 static int	__test_hash_md5(void)
 {
-	t_hash	md5;
-	t_iodes	iodes;
-
-	if (SSL_OK != io_fopen(&iodes, IO_READ|IO_FILE, __small_text_file_path)) {
+	t_ostring in;
+	if (SSL_OK != file_read_all(__small_text_file_path, &in)) {
 		TEST_LOG(ERROR, FILE_READ_ERROR);
-		return (SSL_ERR);
+		TEST_FAIL();
 	}
-	hash_md5_init(&md5);
-	hash_md5_update_stream(&md5, &iodes);
-	hash_md5_final(&md5);
-	io_fclose(&iodes);
 
-	TEST_ASSERT(md5.hashsize == MD5_HASH_SIZE);
-	TEST_ASSERT(ft_strneq(ft_bytes_to_hex(md5.hash, MD5_HASH_SIZE), __small_text_md5_hash_hex, MD5_HASH_SIZE * 2));
+	t_hash ctx = {0};
+	hash_md5_init(&ctx);
+	TEST_ASSERT(ctx.blocksize == MD5_BLOCK_SIZE);
+	TEST_ASSERT(ctx.hashsize == MD5_HASH_SIZE);
 
-	if (SSL_OK != io_fopen(&iodes, IO_READ|IO_FILE, __large_text_file_path)) {
+	size_t rbytes = 0;
+	while (rbytes + MD5_BLOCK_SIZE <= in.size) {
+		md5_update_block(&ctx, in.content + rbytes);
+		rbytes += MD5_BLOCK_SIZE;
+	}
+	md5_final_block(&ctx, in.content + rbytes, in.size - rbytes);
+
+	char *hex = ft_bytes_to_hex(ctx.hash, ctx.hashsize);
+	TEST_ASSERT(ft_streq(hex, __small_text_md5_hash_hex));
+
+	TEST_PASS();
+}
+
+static int	__test_hash_md5_transform(void)
+{
+	t_ostring input;
+	if (SSL_OK != file_read_all(__large_text_file_path, &input)) {
 		TEST_LOG(ERROR, FILE_READ_ERROR);
-		return (SSL_ERR);
+		TEST_FAIL();
 	}
-	hash_md5_init(&md5);
-	hash_md5_update_stream(&md5, &iodes);
-	hash_md5_final(&md5);
-	io_fclose(&iodes);
 
-	TEST_ASSERT(md5.hashsize == MD5_HASH_SIZE);
-	TEST_ASSERT(ft_strneq(ft_bytes_to_hex(md5.hash, MD5_HASH_SIZE), __large_text_md5_hash_hex, MD5_HASH_SIZE * 2));
+	size_t insize = input.size;
+	uint8_t *in = input.content;
+	size_t outsize = insize;
+	uint8_t	out[insize];
+
+	t_hash ctx = {0};
+	hash_md5_init(&ctx);
+	TEST_ASSERT(ctx.blocksize == MD5_BLOCK_SIZE);
+	TEST_ASSERT(ctx.hashsize == MD5_HASH_SIZE);
+
+	// update
+	t_transform_result result = {0};
+	ssize_t tconsumed = 0, tproduced = 0;
+	while (1) {
+ 		result = md5_transform_update(&ctx, in+tconsumed, insize-tconsumed, out+tproduced, outsize-tproduced);
+ 		if (result.status == TRANSFORM_ERROR) {
+   			break;
+   		}
+     	tconsumed += result.consumed;
+     	tproduced += result.produced;
+   		if (result.status != TRANSFORM_OK) {
+     		break;
+     	}
+	}
+	TEST_ASSERT(result.status != TRANSFORM_ERROR);
+	TEST_ASSERT(tconsumed == insize);
+	TEST_ASSERT(tproduced == insize);
+
+	// final
+	while (1) {
+ 		result = md5_transform_final(&ctx, in+tconsumed, insize-tconsumed, out+tproduced, outsize-tproduced);
+   		if (result.status == TRANSFORM_ERROR) {
+     		break;
+     	}
+     	tconsumed += result.consumed;
+     	tproduced += result.produced;
+  		if (result.status != TRANSFORM_OK) {
+      		break;
+      	}
+	}
+	TEST_ASSERT(result.status == TRANSFORM_DONE);
+	TEST_ASSERT(tconsumed == insize);
+	TEST_ASSERT(tproduced == insize);
+
+	char *hex = ft_bytes_to_hex(ctx.hash, ctx.hashsize);
+	TEST_ASSERT(ft_streq(hex, __large_text_md5_hash_hex));
+
 	TEST_PASS();
 }
 
 static int	__test_hash_sha1(void)
 {
-	t_hash	sha1;
-	t_iodes	iodes;
-
-	if (SSL_OK != io_fopen(&iodes, IO_READ|IO_FILE, __small_text_file_path)) {
+	t_ostring in;
+	if (SSL_OK != file_read_all(__small_text_file_path, &in)) {
 		TEST_LOG(ERROR, FILE_READ_ERROR);
-		return (SSL_ERR);
+		TEST_FAIL();
 	}
-	hash_sha1_init(&sha1);
-	hash_sha1_update_stream(&sha1, &iodes);
-	hash_sha1_final(&sha1);
-	io_fclose(&iodes);
 
-	TEST_ASSERT(sha1.hashsize == SHA1_HASH_SIZE);
-	TEST_ASSERT(ft_strneq(ft_bytes_to_hex(sha1.hash, SHA1_HASH_SIZE), __small_text_sha1_hash_hex, SHA1_HASH_SIZE * 2));
+	t_hash ctx = {0};
+	hash_sha1_init(&ctx);
+	TEST_ASSERT(ctx.blocksize == SHA1_BLOCK_SIZE);
+	TEST_ASSERT(ctx.hashsize == SHA1_HASH_SIZE);
 
-	if (SSL_OK != io_fopen(&iodes, IO_READ|IO_FILE, __large_text_file_path)) {
-		TEST_LOG(ERROR, FILE_READ_ERROR);
-		return (SSL_ERR);
+	size_t rbytes = 0;
+	while (rbytes + SHA1_BLOCK_SIZE <= in.size) {
+		sha1_update_block(&ctx, in.content + rbytes);
+		rbytes += SHA1_BLOCK_SIZE;
 	}
-	hash_sha1_init(&sha1);
-	hash_sha1_update_stream(&sha1, &iodes);
-	hash_sha1_final(&sha1);
-	io_fclose(&iodes);
+	sha1_final_block(&ctx, in.content + rbytes, in.size - rbytes);
 
-	TEST_ASSERT(sha1.hashsize == SHA1_HASH_SIZE);
-	TEST_ASSERT(ft_strneq(ft_bytes_to_hex(sha1.hash, SHA1_HASH_SIZE), __large_text_sha1_hash_hex, SHA1_HASH_SIZE * 2));
+	char *hex = ft_bytes_to_hex(ctx.hash, ctx.hashsize);
+	TEST_ASSERT(ft_streq(hex, __small_text_sha1_hash_hex));
+
 	TEST_PASS();
 }
 
-static int	__test_hash_sha224(void)
+static int	__test_hash_sha1_transform(void)
 {
-	t_hash	sha224;
-	t_iodes	iodes;
-
-	if (SSL_OK != io_fopen(&iodes, IO_READ|IO_FILE, __small_text_file_path)) {
+	t_ostring input;
+	if (SSL_OK != file_read_all(__large_text_file_path, &input)) {
 		TEST_LOG(ERROR, FILE_READ_ERROR);
-		return (SSL_ERR);
+		TEST_FAIL();
 	}
-	hash_sha224_init(&sha224);
-	hash_sha224_update_stream(&sha224, &iodes);
-	hash_sha224_final(&sha224);
-	io_fclose(&iodes);
 
-	TEST_ASSERT(sha224.hashsize == SHA224_HASH_SIZE);
-	TEST_ASSERT(ft_strneq(ft_bytes_to_hex(sha224.hash, SHA224_HASH_SIZE), __small_text_sha224_hash_hex, SHA224_HASH_SIZE * 2));
+	size_t insize = input.size;
+	uint8_t *in = input.content;
+	size_t outsize = insize;
+	uint8_t	out[insize];
 
-	if (SSL_OK != io_fopen(&iodes, IO_READ|IO_FILE, __large_text_file_path)) {
-		TEST_LOG(ERROR, FILE_READ_ERROR);
-		return (SSL_ERR);
+	t_hash ctx = {0};
+	hash_sha1_init(&ctx);
+	TEST_ASSERT(ctx.blocksize == SHA1_BLOCK_SIZE);
+	TEST_ASSERT(ctx.hashsize == SHA1_HASH_SIZE);
+
+	// update
+	t_transform_result result = {0};
+	ssize_t tconsumed = 0, tproduced = 0;
+	while (1) {
+ 		result = sha1_transform_update(&ctx, in+tconsumed, insize-tconsumed, out+tproduced, outsize-tproduced);
+ 		if (result.status == TRANSFORM_ERROR) {
+   			break;
+   		}
+     	tconsumed += result.consumed;
+     	tproduced += result.produced;
+   		if (result.status != TRANSFORM_OK) {
+     		break;
+     	}
 	}
-	hash_sha224_init(&sha224);
-	hash_sha224_update_stream(&sha224, &iodes);
-	hash_sha224_final(&sha224);
-	io_fclose(&iodes);
+	TEST_ASSERT(result.status != TRANSFORM_ERROR);
+	TEST_ASSERT(tconsumed == insize);
+	TEST_ASSERT(tproduced == insize);
 
-	TEST_ASSERT(sha224.hashsize == SHA224_HASH_SIZE);
-	TEST_ASSERT(ft_strneq(ft_bytes_to_hex(sha224.hash, SHA224_HASH_SIZE), __large_text_sha224_hash_hex, SHA224_HASH_SIZE * 2));
+	// final
+	while (1) {
+ 		result = sha1_transform_final(&ctx, in+tconsumed, insize-tconsumed, out+tproduced, outsize-tproduced);
+   		if (result.status == TRANSFORM_ERROR) {
+     		break;
+     	}
+     	tconsumed += result.consumed;
+     	tproduced += result.produced;
+  		if (result.status != TRANSFORM_OK) {
+      		break;
+      	}
+	}
+	TEST_ASSERT(result.status == TRANSFORM_DONE);
+	TEST_ASSERT(tconsumed == insize);
+	TEST_ASSERT(tproduced == insize);
+
+	char *hex = ft_bytes_to_hex(ctx.hash, ctx.hashsize);
+	TEST_ASSERT(ft_streq(hex, __large_text_sha1_hash_hex));
+
 	TEST_PASS();
 }
 
 static int	__test_hash_sha256(void)
 {
-	t_hash	sha256;
-	t_iodes	iodes;
-
-	if (SSL_OK != io_fopen(&iodes, IO_READ|IO_FILE, __small_text_file_path)) {
+	t_ostring in;
+	if (SSL_OK != file_read_all(__small_text_file_path, &in)) {
 		TEST_LOG(ERROR, FILE_READ_ERROR);
-		return (SSL_ERR);
+		TEST_FAIL();
 	}
-	hash_sha256_init(&sha256);
-	hash_sha256_update_stream(&sha256, &iodes);
-	hash_sha256_final(&sha256);
-	io_fclose(&iodes);
 
-	TEST_ASSERT(sha256.hashsize == SHA256_HASH_SIZE);
-	TEST_ASSERT(ft_strneq(ft_bytes_to_hex(sha256.hash, SHA256_HASH_SIZE), __small_text_sha256_hash_hex, SHA256_HASH_SIZE * 2));
+	t_hash ctx = {0};
+	hash_sha256_init(&ctx);
+	TEST_ASSERT(ctx.blocksize == SHA256_BLOCK_SIZE);
+	TEST_ASSERT(ctx.hashsize == SHA256_HASH_SIZE);
 
-	if (SSL_OK != io_fopen(&iodes, IO_READ|IO_FILE, __large_text_file_path)) {
-		TEST_LOG(ERROR, FILE_READ_ERROR);
-		return (SSL_ERR);
+	size_t rbytes = 0;
+	while (rbytes + SHA256_BLOCK_SIZE <= in.size) {
+		sha256_update_block(&ctx, in.content + rbytes);
+		rbytes += SHA256_BLOCK_SIZE;
 	}
-	hash_sha256_init(&sha256);
-	hash_sha256_update_stream(&sha256, &iodes);
-	hash_sha256_final(&sha256);
-	io_fclose(&iodes);
+	sha256_final_block(&ctx, in.content + rbytes, in.size - rbytes);
 
-	TEST_ASSERT(sha256.hashsize == SHA256_HASH_SIZE);
-	TEST_ASSERT(ft_strneq(ft_bytes_to_hex(sha256.hash, SHA256_HASH_SIZE), __large_text_sha256_hash_hex, SHA256_HASH_SIZE * 2));
+	char *hex = ft_bytes_to_hex(ctx.hash, ctx.hashsize);
+	TEST_ASSERT(ft_streq(hex, __small_text_sha256_hash_hex));
+
 	TEST_PASS();
 }
 
-static int	__test_hash_sha384(void)
+static int	__test_hash_sha256_transform(void)
 {
-	t_hash	sha384;
-	t_iodes	iodes;
-
-	if (SSL_OK != io_fopen(&iodes, IO_READ|IO_FILE, __small_text_file_path)) {
+	t_ostring input;
+	if (SSL_OK != file_read_all(__large_text_file_path, &input)) {
 		TEST_LOG(ERROR, FILE_READ_ERROR);
-		return (SSL_ERR);
+		TEST_FAIL();
 	}
-	hash_sha384_init(&sha384);
-	hash_sha384_update_stream(&sha384, &iodes);
-	hash_sha384_final(&sha384);
-	io_fclose(&iodes);
 
-	TEST_ASSERT(sha384.hashsize == SHA384_HASH_SIZE);
-	TEST_ASSERT(ft_strneq(ft_bytes_to_hex(sha384.hash, SHA384_HASH_SIZE), __small_text_sha384_hash_hex, SHA384_HASH_SIZE * 2));
+	size_t insize = input.size;
+	uint8_t *in = input.content;
+	size_t outsize = insize;
+	uint8_t	out[insize];
 
-	if (SSL_OK != io_fopen(&iodes, IO_READ|IO_FILE, __large_text_file_path)) {
+	t_hash ctx = {0};
+	hash_sha256_init(&ctx);
+	TEST_ASSERT(ctx.blocksize == SHA256_BLOCK_SIZE);
+	TEST_ASSERT(ctx.hashsize == SHA256_HASH_SIZE);
+
+	// update
+	t_transform_result result = {0};
+	ssize_t tconsumed = 0, tproduced = 0;
+	while (1) {
+ 		result = sha256_transform_update(&ctx, in+tconsumed, insize-tconsumed, out+tproduced, outsize-tproduced);
+ 		if (result.status == TRANSFORM_ERROR) {
+   			break;
+   		}
+     	tconsumed += result.consumed;
+     	tproduced += result.produced;
+   		if (result.status != TRANSFORM_OK) {
+     		break;
+     	}
+	}
+	TEST_ASSERT(result.status != TRANSFORM_ERROR);
+	TEST_ASSERT(tconsumed == insize);
+	TEST_ASSERT(tproduced == insize);
+
+	// final
+	while (1) {
+ 		result = sha256_transform_final(&ctx, in+tconsumed, insize-tconsumed, out+tproduced, outsize-tproduced);
+   		if (result.status == TRANSFORM_ERROR) {
+     		break;
+     	}
+     	tconsumed += result.consumed;
+     	tproduced += result.produced;
+  		if (result.status != TRANSFORM_OK) {
+      		break;
+      	}
+	}
+	TEST_ASSERT(result.status == TRANSFORM_DONE);
+	TEST_ASSERT(tconsumed == insize);
+	TEST_ASSERT(tproduced == insize);
+
+	char *hex = ft_bytes_to_hex(ctx.hash, ctx.hashsize);
+	TEST_ASSERT(ft_streq(hex, __large_text_sha256_hash_hex));
+
+	TEST_PASS();
+}
+
+static int	__test_hash_sha224_transform(void)
+{
+	t_ostring input;
+	if (SSL_OK != file_read_all(__large_text_file_path, &input)) {
 		TEST_LOG(ERROR, FILE_READ_ERROR);
-		return (SSL_ERR);
+		TEST_FAIL();
 	}
-	hash_sha384_init(&sha384);
-	hash_sha384_update_stream(&sha384, &iodes);
-	hash_sha384_final(&sha384);
-	io_fclose(&iodes);
 
-	TEST_ASSERT(sha384.hashsize == SHA384_HASH_SIZE);
-	TEST_ASSERT(ft_strneq(ft_bytes_to_hex(sha384.hash, SHA384_HASH_SIZE), __large_text_sha384_hash_hex, SHA384_HASH_SIZE * 2));
+	size_t insize = input.size;
+	uint8_t *in = input.content;
+	size_t outsize = insize;
+	uint8_t	out[insize];
+
+	t_hash ctx = {0};
+	hash_sha224_init(&ctx);
+	TEST_ASSERT(ctx.blocksize == SHA256_BLOCK_SIZE);
+	TEST_ASSERT(ctx.hashsize == SHA224_HASH_SIZE);
+
+	// update
+	t_transform_result result = {0};
+	ssize_t tconsumed = 0, tproduced = 0;
+	while (1) {
+ 		result = sha224_transform_update(&ctx, in+tconsumed, insize-tconsumed, out+tproduced, outsize-tproduced);
+ 		if (result.status == TRANSFORM_ERROR) {
+   			break;
+   		}
+     	tconsumed += result.consumed;
+     	tproduced += result.produced;
+   		if (result.status != TRANSFORM_OK) {
+     		break;
+     	}
+	}
+	TEST_ASSERT(result.status != TRANSFORM_ERROR);
+	TEST_ASSERT(tconsumed == insize);
+	TEST_ASSERT(tproduced == insize);
+
+	// final
+	while (1) {
+ 		result = sha224_transform_final(&ctx, in+tconsumed, insize-tconsumed, out+tproduced, outsize-tproduced);
+   		if (result.status == TRANSFORM_ERROR) {
+     		break;
+     	}
+     	tconsumed += result.consumed;
+     	tproduced += result.produced;
+  		if (result.status != TRANSFORM_OK) {
+      		break;
+      	}
+	}
+	TEST_ASSERT(result.status == TRANSFORM_DONE);
+	TEST_ASSERT(tconsumed == insize);
+	TEST_ASSERT(tproduced == insize);
+
+	char *hex = ft_bytes_to_hex(ctx.hash, ctx.hashsize);
+	TEST_ASSERT(ft_streq(hex, __large_text_sha224_hash_hex));
+
 	TEST_PASS();
 }
 
 static int	__test_hash_sha512(void)
 {
-	t_hash	sha512;
-	t_iodes	iodes;
-
-	if (SSL_OK != io_fopen(&iodes, IO_READ|IO_FILE, __small_text_file_path)) {
+	t_ostring in;
+	if (SSL_OK != file_read_all(__small_text_file_path, &in)) {
 		TEST_LOG(ERROR, FILE_READ_ERROR);
-		return (SSL_ERR);
+		TEST_FAIL();
 	}
-	hash_sha512_init(&sha512);
-	hash_sha512_update_stream(&sha512, &iodes);
-	hash_sha512_final(&sha512);
-	io_fclose(&iodes);
 
-	TEST_ASSERT(sha512.hashsize == SHA512_HASH_SIZE);
-	TEST_ASSERT(ft_strneq(ft_bytes_to_hex(sha512.hash, SHA512_HASH_SIZE), __small_text_sha512_hash_hex, SHA512_HASH_SIZE * 2));
+	t_hash ctx = {0};
+	hash_sha512_init(&ctx);
+	TEST_ASSERT(ctx.blocksize == SHA512_BLOCK_SIZE);
+	TEST_ASSERT(ctx.hashsize == SHA512_HASH_SIZE);
 
-	if (SSL_OK != io_fopen(&iodes, IO_READ|IO_FILE, __large_text_file_path)) {
+	size_t rbytes = 0;
+	while (rbytes + SHA512_BLOCK_SIZE <= in.size) {
+		sha512_update_block(&ctx, in.content + rbytes);
+		rbytes += SHA512_BLOCK_SIZE;
+	}
+	sha512_final_block(&ctx, in.content + rbytes, in.size - rbytes);
+
+	char *hex = ft_bytes_to_hex(ctx.hash, ctx.hashsize);
+	TEST_ASSERT(ft_streq(hex, __small_text_sha512_hash_hex));
+
+	TEST_PASS();
+}
+
+static int	__test_hash_sha512_transform(void)
+{
+	t_ostring input;
+	if (SSL_OK != file_read_all(__large_text_file_path, &input)) {
 		TEST_LOG(ERROR, FILE_READ_ERROR);
-		return (SSL_ERR);
+		TEST_FAIL();
 	}
-	hash_sha512_init(&sha512);
-	hash_sha512_update_stream(&sha512, &iodes);
-	hash_sha512_final(&sha512);
-	io_fclose(&iodes);
 
-	TEST_ASSERT(sha512.hashsize == SHA512_HASH_SIZE);
-	TEST_ASSERT(ft_strneq(ft_bytes_to_hex(sha512.hash, SHA512_HASH_SIZE), __large_text_sha512_hash_hex, SHA512_HASH_SIZE * 2));
+	size_t insize = input.size;
+	uint8_t *in = input.content;
+	size_t outsize = insize;
+	uint8_t	out[insize];
+
+	t_hash ctx = {0};
+	hash_sha512_init(&ctx);
+	TEST_ASSERT(ctx.blocksize == SHA512_BLOCK_SIZE);
+	TEST_ASSERT(ctx.hashsize == SHA512_HASH_SIZE);
+
+	// update
+	t_transform_result result = {0};
+	ssize_t tconsumed = 0, tproduced = 0;
+	while (1) {
+ 		result = sha512_transform_update(&ctx, in+tconsumed, insize-tconsumed, out+tproduced, outsize-tproduced);
+ 		if (result.status == TRANSFORM_ERROR) {
+   			break;
+   		}
+     	tconsumed += result.consumed;
+     	tproduced += result.produced;
+   		if (result.status != TRANSFORM_OK) {
+     		break;
+     	}
+	}
+	TEST_ASSERT(result.status != TRANSFORM_ERROR);
+	TEST_ASSERT(tconsumed == insize);
+	TEST_ASSERT(tproduced == insize);
+
+	// final
+	while (1) {
+ 		result = sha512_transform_final(&ctx, in+tconsumed, insize-tconsumed, out+tproduced, outsize-tproduced);
+   		if (result.status == TRANSFORM_ERROR) {
+     		break;
+     	}
+     	tconsumed += result.consumed;
+     	tproduced += result.produced;
+  		if (result.status != TRANSFORM_OK) {
+      		break;
+      	}
+	}
+	TEST_ASSERT(result.status == TRANSFORM_DONE);
+	TEST_ASSERT(tconsumed == insize);
+	TEST_ASSERT(tproduced == insize);
+
+	char *hex = ft_bytes_to_hex(ctx.hash, ctx.hashsize);
+	TEST_ASSERT(ft_streq(hex, __large_text_sha512_hash_hex));
+
+	TEST_PASS();
+}
+
+static int	__test_hash_sha384_transform(void)
+{
+	t_ostring input;
+	if (SSL_OK != file_read_all(__large_text_file_path, &input)) {
+		TEST_LOG(ERROR, FILE_READ_ERROR);
+		TEST_FAIL();
+	}
+
+	size_t insize = input.size;
+	uint8_t *in = input.content;
+	size_t outsize = insize;
+	uint8_t	out[insize];
+
+	t_hash ctx = {0};
+	hash_sha384_init(&ctx);
+	TEST_ASSERT(ctx.blocksize == SHA512_BLOCK_SIZE);
+	TEST_ASSERT(ctx.hashsize == SHA384_HASH_SIZE);
+
+	// update
+	t_transform_result result = {0};
+	ssize_t tconsumed = 0, tproduced = 0;
+	while (1) {
+ 		result = sha384_transform_update(&ctx, in+tconsumed, insize-tconsumed, out+tproduced, outsize-tproduced);
+ 		if (result.status == TRANSFORM_ERROR) {
+   			break;
+   		}
+     	tconsumed += result.consumed;
+     	tproduced += result.produced;
+   		if (result.status != TRANSFORM_OK) {
+     		break;
+     	}
+	}
+	TEST_ASSERT(result.status != TRANSFORM_ERROR);
+	TEST_ASSERT(tconsumed == insize);
+	TEST_ASSERT(tproduced == insize);
+
+	// final
+	while (1) {
+ 		result = sha384_transform_final(&ctx, in+tconsumed, insize-tconsumed, out+tproduced, outsize-tproduced);
+   		if (result.status == TRANSFORM_ERROR) {
+     		break;
+     	}
+     	tconsumed += result.consumed;
+     	tproduced += result.produced;
+  		if (result.status != TRANSFORM_OK) {
+      		break;
+      	}
+	}
+	TEST_ASSERT(result.status == TRANSFORM_DONE);
+	TEST_ASSERT(tconsumed == insize);
+	TEST_ASSERT(tproduced == insize);
+
+	char *hex = ft_bytes_to_hex(ctx.hash, ctx.hashsize);
+	TEST_ASSERT(ft_streq(hex, __large_text_sha384_hash_hex));
+
 	TEST_PASS();
 }
