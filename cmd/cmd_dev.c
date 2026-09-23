c78194255b20201d1357621545b254b9f9af4f1c
Rail Sharipov <hashan.hall@gmail.com>
2026-09-22 18:44:10 -0600

    Update cmd_dev.c

--- cmd/cmd_dev.c
+++ cmd/cmd_dev.c
@@ -16,8 +16,8 @@
 
 int	cmd_dev(const t_cmd *cmd)
 {
-	t_json_v2 *jschema = NULL;
-	if (JSON_OK != json_v2_parse_file("resources/asn1/schema-v2-example.json", &jschema)) {
+	const t_json_v2 *jschema = NULL;
+	if (JSON_OK != json_v2_parse_file("resources/asn1/schema-v2-pkcs8-KeyContainers.json", &jschema)) {
 		SSL_LOG(ERROR, "json parse error");
 		return (SSL_ERR);
 	}
@@ -25,6 +25,9 @@ int	cmd_dev(const t_cmd *cmd)
 		SSL_LOG(ERROR, "invalid json");
 		return (SSL_ERR);
 	}
+
+	// char *json_dumps = json_v2_pretty_dumps(jschema, 2, 240, true);
+	// ft_printf("%s\n", json_dumps);
 
 	if (SSL_OK != asn1_v2_schema_validate(jschema)) {
 		SSL_LOG(ERROR, "invalid asn1 schema");
@@ -41,9 +44,22 @@ int	cmd_dev(const t_cmd *cmd)
 		SSL_LOG(INFO, "asn1 schema parse success");
 	}
 
-	char *dumps = asn1_v2_module_dumps(asn1_module);
+	// char *atype_dumps = asn1_v2_module_pretty_dumps(asn1_module, 1, 320, true);
+	// ft_printf("%s\n", atype_dumps);
 
-	ft_printf("%s\n", dumps);
+	t_asn_v2_type *asn1_type = ft_htbl_v2_get(&asn1_module->types, "PrivateKeyInfo");
+	assert(NULL != asn1_type);
+
+	t_der_v2_type *der_type = NULL;
+	if (SSL_OK != asn1_v2_type_compile(&der_type, asn1_type)) {
+		SSL_LOG(ERROR, "asn1 type compile failed");
+		return (SSL_ERR);
+	} else {
+		SSL_LOG(INFO, "asn1 type compile success");
+	}
+
+	char *dtype_dumps = der_v2_type_pretty_dumps(der_type, 0, 320, true);
+	ft_printf("%s\n", dtype_dumps);
 
 	return (SSL_OK);
 }
